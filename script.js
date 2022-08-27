623a5b0ebba121179fc65c5308faac5cd0f71908
ejjubd <ejjubdemir@hotmail.com>
2022-08-27 19:49:23 +0300

    added css, ready for a script

---
+++ script.js
@@ -0,0 +1,44 @@
+function add (a,b){
+    return a + b ;
+}
+
+function substract(a,b) {
+    return a - b ;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a/b
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const person = {
+    name: "Umejr",
+    lastName: "Demir",
+    age:22,
+    brothers: ["Ejjub", "Velid", "Abdullah"],
+    sisters: ["Sara"]
+};
+
+console.log(person.age);
+console.log(person.lastName);
+console.log(person.name);
+
+const person2 = {...person, name: "iahsfoia"}
+
+*/

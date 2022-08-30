38bb5722f460ba6023a85b47b59bc19d529f0f39
ejjubd <ejjubdemir@hotmail.com>
2022-08-30 19:01:54 +0300

    added functionality for the upper screen

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let a = ''
 let b = ''
 
 function add (a,b){
-    return a + b ;
+    return a+b
 }
 
 function substract(a,b) {
-    return a - b ;
+    return a-b
 }
 
 function multiply(a,b) {
-    return a * b;
+    return a*b
 }
 
 function divide (a,b) {
@@ -19,8 +19,8 @@ function divide (a,b) {
 
 
 function operate(operator,a,b){
-    a = Number (a);
-    b = Number (b);
+    a = parseInt(a)
+    b = parseInt(b)
     switch (operator) {
         case '-':
             return substract(a,b);
@@ -34,55 +34,71 @@ function operate(operator,a,b){
     }
 }
 
+let n = ""
+const operators = ["/", "*", "+", "-"]
+let shouldCalculate = false
+let operator = ""
+let temp = ""
+let calc = 0
 
+function getNumber(number) {
 
+    if(operators.includes(number)){
+        
+        if(calc) {
+            document.getElementById('upper-screen').innerHTML = calc+ " " +number
+            temp = calc
+        }
+        else {
+            document.getElementById('upper-screen').innerHTML = n+" "+number
+            temp = n
+        }
+        shouldCalculate = false
+        operator = number
+        n = ""
+        document.getElementById('down-screen').innerHTML = n
 
+    } else if(shouldCalculate){
+        
+        calc += operate(operator, temp, number)
+        document.getElementById('upper-screen').innerHTML = calc
+        temp = ""
+        shouldCalculate = false
 
+    } else {
+        n+=number
+        document.getElementById('down-screen').innerHTML = n
+    }
+    
 
-
-
-
-
-
-
-
-
-
-function clear () {
-    const clear = document.querySelector('#clear')
-    clear.addEventListener('click', function() {
-    document.getElementById('down-screen').innerHTML = '0'
-
-})
 }
 
 
-function work(){
-    deleteButton();
 
-  clear();
-  
+
+
+
+
+
+function clearScreen() {
+    n = ''
+    temp = ""
+    calc=0
+   document.getElementById('down-screen').innerHTML = '0'
+   document.getElementById('upper-screen').innerHTML = ''
 }
 
-
-
-function clear () {
-    const clear = document.querySelector('#clear')
-    clear.addEventListener('click', function() {
-    document.getElementById('down-screen').innerHTML = '0'
-
-})
-}
 
 function deleteButton () {
- const deletes = document.querySelector('#delete')
- deletes.addEventListener('click', function(){
-    document.getElementById('down-screen').textContent = document.getElementById('down-screen').textContent.slice(0, -1)
-    
- })
-}
+   
+    n = n.slice(0,-1)
+    document.getElementById('down-screen').innerHTML = n
+   
+    }
+   
 
-work();
+
+
 
 
 
